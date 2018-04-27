a9652f06fa10a088c2aaf9d0f0fcd57d7a1fab05
CMichels <chrisjohnmichels@gmail.com>
2018-04-27 11:16:51 -0500

    update gif src

--- Hero.js
+++ Hero.js
@@ -4,7 +4,7 @@ class Hero extends Person {
     }
 
     render(){
-        let img = "img/dino.gif";
+        let img = "img/Dino.gif";
         let heroDiv = document.getElementsByClassName('hero');
         heroDiv[0].innerHTML = `<h1>${this.name}</h1><img class='heroImg' src="${img}" height='300px' width='300px'><br><h2>Hero's Health: ${this.health}</h2><button class='btn btn-primary' id='heroButton'>Attack!</button>`;
         document.getElementById('heroButton').addEventListener('click', function(){
