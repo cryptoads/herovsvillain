f3d5f3bd2efbe609803e76b6ba9674215aa322bc
CMichels <chrisjohnmichels@gmail.com>
2018-04-27 11:47:05 -0400

    Chaning the render functions  and adding a game state check

--- Hero.js
+++ Hero.js
@@ -4,9 +4,15 @@ class Hero extends Person {
     }
 
     render(){
-        console.log('working');
-        let img = "http://www.wodenworks.com/wp-content/uploads/2017/02/true-700x450.jpeg";
+        let img = "img/dino.gif";
         let heroDiv = document.getElementsByClassName('hero');
-        heroDiv[0].innerHTML = `<img src="${img}" width='300px'><br><h2>Hero's Health: ${this.health}</h2><button id='heroButton'>Attack!</button>`;
+        heroDiv[0].innerHTML = `<h1>${this.name}</h1><img class='heroImg' src="${img}" height='300px' width='300px'><br><h2>Hero's Health: ${this.health}</h2><button class='btn btn-primary' id='heroButton'>Attack!</button>`;
+        document.getElementById('heroButton').addEventListener('click', function(){
+            hero.attack(villain);
+            villain.render();
+            var imgs = document.getElementsByClassName('villainImg');
+            imgs[0].classList.add('apply-shake')
+            healthCheck();
+        })
     }
 }
