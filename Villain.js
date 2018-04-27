46fe5b80a2178a71e335d885f976c4b6f677f34b
CMichels <chrisjohnmichels@gmail.com>
2018-04-27 11:49:55 -0400

    Changing render and gamestate

--- Villain.js
+++ Villain.js
@@ -1,12 +1,18 @@
 class Villain extends Person{
     constructor(){
-     super();   
+     super();
+    }
+        render(){
+        let img = "img/Dennis.gif";
+        let villainDiv = document.getElementsByClassName('villain');
+        villainDiv[0].innerHTML = `<h1>${this.name}</h1><img class= 'villainImg' src="${img}" height='300px' width='300px'><br><h2>Villain's Health: ${this.health}</h2><button class='btn btn-primary' id='villainButton'>Attack!</button>`;
+        document.getElementById('villainButton').addEventListener('click', function(){
+            villain.attack(hero);
+            hero.render();
+            var imgs = document.getElementsByClassName('heroImg');
+            imgs[0].classList.add('apply-shake')
+            healthCheck();
+        })
     }
 
-        render(){
-        let img = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Villainc.svg/1024px-Villainc.svg.png";
-        let villainDiv = document.getElementsByClassName('villain');
-        villainDiv[0].innerHTML = `<img src="${img}" width='300px'><br><h2>Villain's Health: ${this.health}</h2><button id='villainButton'>Attack!</button>`;
-    }
-   
 }
